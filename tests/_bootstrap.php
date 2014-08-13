73a0f92e66c9b9ff949a8295f08ad13effd337fe
antivariant <antivariant@mail.ru>
2014-08-13 23:29:00 +0300

    настроил тест бд.

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,2 +1,5 @@
 <?php
 // This is global bootstrap for autoloading 
+echo exec('mysql --user="root" --password="masterkey" robozap<c:/Sites/robozap/tests/_data/dump.sql');
+
+
---
+++ tests/acceptance/RegistrationCept.php
@@ -0,0 +1,15 @@
+<?php 
+$I = new AcceptanceTester($scenario);
+$I->wantTo('залогиниться');
+$I->amOnPage('/');
+$I->click('Войти');
+$I->click('Регистрация');
+$I->fillField(['id'=>'name'],'Codeception test');
+$I->fillField(['id'=>'username'],'ctest');
+$I->fillField(['id'=>'email'],'design@robozap.ru');
+$I->fillField(['id'=>'email2'],'design@robozap.ru');
+$I->fillField(['id'=>'password'],'masterkey');
+$I->fillField(['id'=>'password2'],'masterkey');
+$I->attachFile(['id'=>'image'], 'user.png');
+$I->click('Зарегистрироваться');
+$I->seeInDatabase('robo_users', array('name'=>'Codeception test','username'=>'ctest','email'=>'design@robozap.ru'));
