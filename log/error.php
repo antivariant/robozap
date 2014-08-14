d8475f4c71079b2b347f6aaf68e657af26862768
antivariant <antivariant@mail.ru>
2014-08-14 16:47:05 +0300

    Removed all files that are in the .gitignore

--- log/error.php
+++
@@ -1,9 +0,0 @@
-#
-#<?php die('Forbidden.'); ?>
-#Date: 2014-08-08 19:45:16 UTC
-#Software: Joomla Platform 11.4.0 Stable [ Brian Kernighan ] 03-Jan-2012 00:00 GMT
-
-#Fields: date	time	priority	clientip	category	message
-2014-08-08	19:45:16	INFO	127.0.0.1	Joomla FAILURE: 	Имя пользователя и пароль не совпадают или у вас еще нет учетной записи на сайте
-2014-08-13	18:22:12	INFO	127.0.0.1	Joomla FAILURE: 	Имя пользователя и пароль не совпадают или у вас еще нет учетной записи на сайте
-2014-08-13	18:39:52	INFO	127.0.0.1	Joomla CANCELED: 	
