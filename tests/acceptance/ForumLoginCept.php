606fb14d891829c7432e980f0c30be45da0a50b3
antivariant <admin@robozap.ru>
2014-08-14 23:52:44 +0300

    тест почты closes 056

--- tests/acceptance/ForumLoginCept.php
+++ tests/acceptance/ForumLoginCept.php
@@ -6,4 +6,4 @@ $I->click('Форум');
 $I->fillField(['name'=>'username'],'antivariant');
 $I->fillField(['name'=>'password'],'masterkey');
 $I->click('Войти');
-$I->see('Добро пожаловать','li');
+$I->see('Добро пожаловать','ll');
