029836ca5a0a267980d85970356fb9a6162eef64
antivariant <antivariant@mail.ru>
2014-08-14 22:21:25 +0300

    тест входа в форум closes 056

---
+++ tests/acceptance/ForumLoginCept.php
@@ -0,0 +1,9 @@
+<?php 
+$I = new AcceptanceTester($scenario);
+$I->wantTo('залогиниться в форум на странце форума');
+$I->amOnPage('/');
+$I->click('Форум');
+$I->fillField(['name'=>'username'],'antivariant');
+$I->fillField(['name'=>'password'],'masterkey');
+$I->click('Войти');
+$I->see('Добро пожаловать','li');
